d9bef2ad418bd11a0d024e4043b7865b5de69797
Xatpy <jaime.chapinal@gmail.com>
2016-08-27 19:16:14 +0200

    Prototype

---
+++ Assets/Scripts/AqueductManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AqueductManager : MonoBehaviour {
+
+	List<GameObject> listArcs = new List<GameObject> ();
+	public GameObject FirstArc;
+	public GameObject prefabArc;
+	public Button buttonGo;
+	public GameObject InputSystem;
+	public int CurrentArc = 0;
+	public int TotalArcs = 6;
+
+	// Use this for initialization
+	void Start () {
+		listArcs = new List<GameObject> ();
+		listArcs.Add (FirstArc);
+
+		buttonGo.interactable = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//listArcs[0]
+	}
+
+	void ButtonPressed(bool value) {
+		Debug.Log ("Button Pressed");
+		listArcs[CurrentArc].SendMessage("SetStatus", true);
+	}
+
+	void ButtonReleased(bool value) {
+		Debug.Log ("Button released");
+		listArcs[CurrentArc].SendMessage("SetStatus", false);
+		listArcs.Add((GameObject)CreateNewArc ());
+		CurrentArc++;
+
+		if (CurrentArc > TotalArcs) {
+			buttonGo.interactable = true;
+			InputSystem.SendMessage ("ActiveGoButton", true, SendMessageOptions.RequireReceiver);
+		}
+
+	}
+
+	Object CreateNewArc(){
+		Vector3 newPos = listArcs [CurrentArc].transform.position;
+		newPos.x += 2.0f;
+		newPos.y = -9.0f;
+		newPos.z = 10.0f;
+		return Instantiate(prefabArc, newPos, Quaternion.identity);
+	}
+
+}
---
+++ Assets/Scripts/WaterLogic.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterLogic : MonoBehaviour {
+
+	bool bActive = false;
+	public float forceX = 400.0f;
+
+	private Rigidbody rb;
+
+	void Start() {
+		rb = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (bActive) {
+			
+		}
+	}
+
+	public void GoWater() {
+		Debug.Log ("go Water");
+		bActive = true;
+		Vector3 force = new Vector3 (forceX, -100, 0);
+		rb.AddForce (force);
+		rb.isKinematic = false;
+	}
+}
