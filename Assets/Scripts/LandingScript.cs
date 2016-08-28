5a9f7acc38376a99aef853c763256bdd5ed7882c
Xatpy <jaime.chapinal@gmail.com>
2016-08-28 21:00:17 +0200

    Adding landing scene.

---
+++ Assets/Scripts/LandingScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LandingScript : MonoBehaviour {
+
+	// Use this for initialization
+	public void LoadGame() {
+		Application.LoadLevel("Game");
+	}
+}
